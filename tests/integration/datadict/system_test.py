8eed8b84374aa8b223d0229acba1ffb38330ca5b
Giang Bui <giangbui@uchicago.edu>
2018-10-23 09:38:47 -0500

    fixup

--- tests/integration/datadict/system_test.py
+++
@@ -1,8 +0,0 @@
-
-def test_status_endpoint(client):
-    res = client.get("/_status")
-    assert res.status_code == 200
-
-def test_version_endpoint(client):
-    res = client.get("/_version")
-    assert res.status_code == 200
